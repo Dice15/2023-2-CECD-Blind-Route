f35a2d5cb675579511caaf442a14a715841679ee
Dice <102275981+Dice15@users.noreply.github.com>
2023-08-20 19:39:19 +0900

    test7

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   const handleLoginWithKeycloak = () => {
-    window.location.href = "http://localhost:8081/oauth2/authorization/google";
+    window.location.href = "http://localhost:8081/oauth2/authorization/keycloak";
   };
 
   return (
     <div className="App">
-      <button onClick={handleLoginWithKeycloak}>Login with Keycloak</button>
-    </div>
+      <header className="App-header">
+        <div>
+          <button onClick={handleLoginWithKeycloak} style={{ width: "200px", height: "50px" }}>Login Test</button>
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header >
+    </div >
   );
 }
 
