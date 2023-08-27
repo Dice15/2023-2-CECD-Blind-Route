00e067c07808c0db93b2e05aebfd72e455b3a675
Dice <102275981+Dice15@users.noreply.github.com>
2023-08-27 13:31:20 +0900

    update: eslintrc 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/home/Home';
 
 function App() {
-
-  const handleLoginWithKeycloak = () => {
-    //window.location.href = "http://localhost:8081/oauth2/authorization/google";
-    window.location.href = "https://blindroute-springboot.koyeb.app/oauth2/authorization/google";
-  };
-
   return (
     <div className="App">
       <BrowserRouter>
@@ -21,13 +15,3 @@ function App() {
 }
 
 export default App;
-
-
-/*
-<header className="App-header">
-        <div>
-          <button onClick={handleLoginWithKeycloak} style={{ width: "200px", height: "50px" }}>Login Test</button>
-        </div>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header >
-*/
