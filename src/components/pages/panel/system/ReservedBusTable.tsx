5640233c1d7c088768b4f5159b0c969ae2b1993e
Dice <102275981+Dice15@users.noreply.github.com>
2023-09-21 20:39:19 +0900

    Update ReservedBusTable.tsx

--- src/components/pages/panel/system/ReservedBusTable.tsx
+++ src/components/pages/panel/system/ReservedBusTable.tsx
@@ -66,7 +66,7 @@ export default function ReservedBusTable({ taskState, userRole }: ReservedBusTab
                         console.log(apiData);
                     }
                 });
-                setReservedBusList([]);
+                // setReservedBusList([]);
             }
 
             setStation(null);
